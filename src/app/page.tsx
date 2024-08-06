57f8778ac05e3419fc0b5ae9369f9c076a3972cb
Nicolás Gómez <Nvandres079@gmail.com>
2024-08-06 11:25:03 -0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { navItems } from "@/data";
 import { FloatingNav } from "@/components/ui/floating-navbar";
-import Hero from "@/components/hero";
+
 import Grid from "@/components/grid";
 import RecentProjects from "@/components/recent-projects";
 import Clients from "@/components/clients";
@@ -13,7 +13,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Hero />
+        
         <Grid />
         <RecentProjects />
         <Clients />
