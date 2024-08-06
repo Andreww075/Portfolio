686d08b5ce1cefef979997a7673f1e38d0f89286
Nicolás Gómez <Nvandres079@gmail.com>
2024-08-06 11:44:29 -0500

    .

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./magic-button";
-import { Spotlight } from "./ui/spotlight";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
-      </div>
+      
 
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
